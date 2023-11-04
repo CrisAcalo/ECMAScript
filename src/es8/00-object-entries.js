4664ba0ea05d088d8069105b79773e7af69a91cf
crisacalo <cristianchoacalo@gmail.com>
2023-11-04 01:50:48 -0500

    Knowing how works Object entries and values

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,15 @@
+//Manipulacion de Objetos
+const countries = {
+  MX: "Mexico",
+  CO: "Colombia",
+  CL: "Chile",
+  EC: "Ecuador",
+};
+
+console.log(Object.entries(countries));
+// [
+//   ["MX", "Mexico"],
+//   ["CO", "Colombia"],
+//   ["CL", "Chile"],
+//   ["EC", "Ecuador"],
+// ];
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,9 @@
+const countries = {
+  MX: "Mexico",
+  CO: "Colombia",
+  CL: "Chile",
+  EC: "Ecuador",
+};
+
+console.log(Object.values(countries));
+//[ 'Mexico', 'Colombia', 'Chile', 'Ecuador' ]
