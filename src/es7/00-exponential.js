3184117ca24120e2b56bb974a9cd1bfa349ea17a
crisacalo <cristianchoacalo@gmail.com>
2023-11-03 23:57:31 -0500

    ES7 additions

---
+++ src/es7/00-exponential.js
@@ -0,0 +1,4 @@
+//ES7
+let exponentResult = 2**3;
+console.log(exponentResult);
+
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,8 @@
+//includes() method for arrays
+const numbers = [1,2,3,4,5];
+
+console.log(numbers.includes(3));
+console.log(numbers.includes(7));
+
+const names = ['Cris', 'Juan', 'Pedro', 'Hermenehildo', 'Sazu', 'Pedro'];
+console.log(names.includes('Pedro', 3));
